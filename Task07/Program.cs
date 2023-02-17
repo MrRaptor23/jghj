da39b4b7b9db346819059b768d6f29feaeefbe64
Dima <wisppro228@gmail.com>
2023-02-17 18:24:41 +0300

    first commit

---
+++ Task07/Program.cs
@@ -0,0 +1,16 @@
+﻿// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Write("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+   int lastNumber = number % 10;
+   Console.Write($"Последняя цифра числа  {lastNumber}");
+}
+else 
+  Console.Write("Вы ввели не верное число");
---
+++ Task_008/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число N - ");
+int number = Convert.ToInt32(Console.ReadLine());   
+int index = 1;
+while (index <= number)
+{
+    if (index%2 == 0)
+    {
+       Console.Write($"{index} ");       
+    }
+    index++;
+}
+    
+ 
+
